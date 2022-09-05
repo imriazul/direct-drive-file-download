db789f5509a7f76d0a27b67d8c26361bf20afcfd
Riazul Islam <mofazzal.blog@gmail.com>
2022-09-05 23:17:57 +0600

    modal added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.onload = setup;
 function setup() {
-    document.getElementById("button").addEventListener("click", go);
+    document.getElementById('inputUrl').addEventListener('input', generateLink)
     document.getElementById("output").addEventListener("click", copy);
 }
 
@@ -14,23 +14,25 @@ function setStatus(status, error = false) {
     }
 }
 
-function go() {
-    const urlField = document.getElementById("input");
+function generateLink() {
+    const showLink = document.getElementById('direct-link')
+    const buttonGen = document.getElementById("button")
+    const urlField = document.getElementById("inputUrl");
     var linkId = urlField.value;
     var idExtractor = /\/d\/(.+?)(?:\/|#|\?|$)/;
     var result = idExtractor.exec(linkId);
-    var outputBox = document.getElementById("output");
+    // var outputBox = document.getElementById("output");
 
     // I am testing new things
-    function makeid(renlen) {
-        var result = '';
+    function makeid(rendomlenngth) {
+        var linkText = '';
         var characters = '345abcdefghjk0123456789mnopqrstuvwxyz123456789';
         var charactersLength = characters.length;
-        for (var i = 0; i < renlen; i++) {
-            result += characters.charAt(Math.floor(Math.random() *
+        for (var i = 0; i < rendomlenngth; i++) {
+            linkText += characters.charAt(Math.floor(Math.random() *
                 charactersLength));
         }
-        return result;
+        return linkText;
     }
     const ren1 = makeid(8);
     const ren2 = makeid(4);
@@ -38,32 +40,37 @@ function go() {
     const ren4 = makeid(4);
     const ren5 = makeid(12);
     if (!result) {
-        outputBox.value = "";
+        // outputBox.value = "";
         setStatus("⚠⚠ Invalid URL ⚠⚠", true);
-        outputBox.disabled = true;
+        // outputBox.disabled = true;
+        buttonGen.disabled = true;
         urlField.style.borderColor = "red"
         return;
     }
-    else{
-        urlField.style.borderColor = "green"
+    else {
+        urlField.style.borderColor = "green";
+        buttonGen.disabled = false;
     }
     var finalLink = "https://drive.google.com/u/0/uc?id=" + result[1] + "&export=download&confirm=t&uuid=" + ren1 + ("-") + ren2 + ("-") + ren3 + ("-") + ren4 + ("-") + ren5;
-    outputBox.disabled = false;
-    outputBox.value = finalLink;
-    setStatus("Success! Click the output link to copy it to your clipboard");
+    // outputBox.disabled = false;
+    // outputBox.value = finalLink;
+    showLink.innerText = finalLink;
+    // setStatus("Success! Click the output link to copy it to your clipboard");
 }
 
-function copy() {
-    if (this.disabled) {
-        return;
-    }
+// function copy() {
+//     if (this.disabled) {
+//         return;
+//     }
 
-    this.select();
-    var copied = document.execCommand("copy");
+//     this.select();
+//     var copied = document.execCommand("copy");
 
-    if (copied) {
-        setStatus("Link copied to clipboard!");
-    } else {
-        setStatus("Couldn't copy link to clipboard. Please copy it manually.", true);
-    }
-}
+//     if (copied) {
+//         setStatus("Link copied to clipboard!");
+//     } else {
+//         setStatus("Couldn't copy link to clipboard. Please copy it manually.", true);
+//     }
+// }
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
